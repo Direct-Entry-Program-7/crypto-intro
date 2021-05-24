5a476f43b3cab9be945043685c19b0194c4e4d51
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-05-24 10:27:35 +0530

    Finalize UIs

--- src/controller/DecryptionController.java
+++ src/controller/DecryptionController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class DecryptionController {
+    public TextField txtText;
+    public TextField txtCipherText;
+    public TextField txtKey;
+
+    public void btnDecrypt_OnAction(ActionEvent actionEvent) {
+    }
 }
--- src/controller/EncryptionController.java
+++ src/controller/EncryptionController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class EncryptionController {
+    public TextField txtText;
+    public TextField txtKey;
+    public TextField txtCipherText;
+
+    public void btnEncrypt_OnAction(ActionEvent actionEvent) {
+    }
 }
