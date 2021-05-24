4dba8c0236aba060e23ae8036b8d8f340974c5c2
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-05-24 12:02:40 +0530

    Finalize the project

---
+++ src/CharacterDemo.java
@@ -0,0 +1,28 @@
+public class CharacterDemo {
+
+    public static void main(String[] args) {
+        // boolean 1
+
+        // Integers
+
+        // char - 16
+        // byte - 8  (sign integers)
+
+        // [1][1][1][1][1][1][1][1]
+        // 1 * 2^7 + 1 * 2^6 + 1 * 2^5 + 1 * 2*4 + 1 * 2^3 + 1 * 2^2 + 1 * 2^1 + 1 * 2^0
+        // short - 16 (sign integers)
+        // int - 32 (sign integers)
+        // long - 64 (sign integers)
+
+        /* Floating point numbers */
+
+        // float - 32
+        // double - 64
+
+
+        // UTF-8
+        // UTF-16 (char) =
+        char a = 'z';
+        System.out.println(a + 0);
+    }
+}
--- src/controller/EncryptionController.java
+++ src/controller/EncryptionController.java
@@ -1,7 +1,11 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+
+import java.util.Arrays;
 
 public class EncryptionController {
     public TextField txtText;
@@ -9,5 +13,39 @@ public class EncryptionController {
     public TextField txtCipherText;
 
     public void btnEncrypt_OnAction(ActionEvent actionEvent) {
+        String text = txtText.getText();
+        String key = txtKey.getText();
+
+//        "abc     ".trim() = "abc"
+//        "     abc".trim() = "abc"
+
+        if (text.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please enter valid text", ButtonType.OK).show();
+            txtText.requestFocus();
+            return;
+        }
+
+        if (key.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please enter valid key", ButtonType.OK).show();
+            txtKey.requestFocus();
+            return;
+        }
+
+        text += key;
+        String reversedText = "";
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversedText += text.charAt(i);
+        }
+
+        String cipherText = "";
+        for (int i = 0; i < reversedText.length(); i++) {
+            int code = reversedText.charAt(i);
+            code += 10;
+            char newChar = (char) code;
+            cipherText += newChar;
+        }
+
+        txtCipherText.setText(cipherText);
+
     }
 }
